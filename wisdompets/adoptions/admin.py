44d13f39b3689ff2bdd220b2b00b55ea641cfba3
Caleb Smith <calebsmith@car-booth-11-mac.content.linkedin.biz>
2020-02-27 17:13:49 -0800

    RECORDED 02_07 SUCCESSFULLY

--- wisdompets/adoptions/admin.py
+++ wisdompets/adoptions/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Pet
+
+@admin.register(Pet)
+class PetAdmin(admin.ModelAdmin):
+    list_display = ['name', 'species', 'breed', 'age', 'sex']
--- wisdompets/adoptions/models.py
+++ wisdompets/adoptions/models.py
@@ -14,3 +14,6 @@ class Pet(models.Model):
 
 class Vaccine(models.Model):
     name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
