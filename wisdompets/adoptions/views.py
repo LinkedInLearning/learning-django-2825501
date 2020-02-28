97ca5e570f7d22ea97c7f99a88e1300d2a5c184a
Caleb Smith <calebsmith@car-booth-11-mac.content.linkedin.biz>
2020-02-28 13:02:22 -0800

    RECORDED 03_03 SUCCESSFULLY

--- wisdompets/adoptions/views.py
+++ wisdompets/adoptions/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import Http404
+
+from .models import Pet
 
 def home(request):
-    return HttpResponse('<p>home view</p>')
+    pets = Pet.objects.all()
+    return render(request, 'home.html', {
+        'pets': pets,
+    })
 
 def pet_detail(request, pet_id):
-    return HttpResponse(f'<p>pet_detail view with id {pet_id}</p>')
+    try:
+        pet = Pet.objects.get(id=pet_id)
+    except Pet.DoesNotExist:
+        raise Http404('pet not found')
+    return render(request, 'pet_detail.html', {
+        'pet': pet,
+    })
