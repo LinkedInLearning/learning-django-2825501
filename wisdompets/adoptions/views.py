100bad9b24c8f038f7ac7d0c2cce2fdf0f5af3a1
Caleb Smith <calebsmith@car-booth-11-mac.content.linkedin.biz>
2020-02-28 10:33:07 -0800

    03_02 RECORDED SUCCESSFULLY

--- wisdompets/adoptions/views.py
+++ wisdompets/adoptions/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+    return HttpResponse('<p>home view</p>')
+
+def pet_detail(request, pet_id):
+    return HttpResponse(f'<p>pet_detail view with id {pet_id}</p>')
