187b5016d48f655edf452416933cbd3346565bd5
Owt And About <70513244+Ikemefula@users.noreply.github.com>
2020-12-24 17:46:54 -0600

    Understand URL patterns
    Understand URL patterns

---
+++ wisdompets/adoptions/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+class Pet(models.Model):
+    SEX_CHOICES = [('M', 'Male'), ('F', 'Female')]
+    name = models.CharField(max_length=100)
+    submitter = models.CharField(max_length=100)
+    species = models.CharField(max_length=30)
+    breed = models.CharField(max_length=30, blank=True)
+    description = models.TextField()
+    sex = models.CharField(max_length=1, choices=SEX_CHOICES, blank=True)
+    submission_date = models.DateTimeField()
+    age = models.IntegerField(null=True)
+    vaccinations = models.ManyToManyField('Vaccine', blank=True)
+
+class Vaccine(models.Model):
+    name = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.name
