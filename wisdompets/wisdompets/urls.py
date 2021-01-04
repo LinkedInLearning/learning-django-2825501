b7f765921dceb153d62fd743f996c488a83cb52d
Ikemefula Orisakwe <IkeOrisakwe@gmail.com>
2021-01-04 02:25:41 -0600

    Site Completed

--- wisdompets/wisdompets/urls.py
+++ wisdompets/wisdompets/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from adoptions import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('adoptions/<int:pet_id>/', views.pet_detail, name='pet_detail'),
 ]
